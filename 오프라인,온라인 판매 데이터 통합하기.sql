aa6c1485ebf36037b09481b14b9db38fea3b8d76
jeongwusi <jeongwusi@gmail.com>
2024-02-03 22:36:53 +0900

    📝 docs: ORDER BY 절에 대해서 설명 추가

--- 오프라인,온라인 판매 데이터 통합하기.sql
+++ 오프라인,온라인 판매 데이터 통합하기.sql
@@ -19,4 +19,6 @@ ORDER BY 1, 2, 3;
 
 -- 결과를 날짜, 제품 ID, 사용자 ID에 따라 정렬합니다.
 
+-- ORDER BY 절은 전체 결과 집합에 대해 적용되므로, 첫 번째 SELECT 문이거나 두 번째 SELECT 문이거나 모두 동일하게 적용된다. (오히려 각각의 SELECT 문에 ORDER BY를 사용하면 오류가 나온다.)
+
 -- https://school.programmers.co.kr/learn/courses/30/lessons/131537
