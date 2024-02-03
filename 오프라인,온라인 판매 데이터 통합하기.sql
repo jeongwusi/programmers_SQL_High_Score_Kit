aaac74f8c9e3a6d0fd33f3112e10ec5566488967
jeongwusi <jeongwusi@gmail.com>
2024-02-03 22:20:33 +0900

    ✨ feat: `오프라인/온라임 판매 데이터 통합하기` 문제 해결

---
+++ 오프라인,온라인 판매 데이터 통합하기.sql
@@ -0,0 +1,22 @@
+SELECT DATE_FORMAT(SALES_DATE, '%Y-%m-%d') AS SALES_DATE, PRODUCT_ID, USER_ID, SALES_AMOUNT
+FROM ONLINE_SALE
+WHERE MONTH(SALES_DATE) = 03
+UNION
+SELECT DATE_FORMAT(SALES_DATE, '%Y-%m-%d') AS SALES_DATE, PRODUCT_ID, NULL AS USER_ID, SALES_AMOUNT
+FROM OFFLINE_SALE
+WHERE MONTH(SALES_DATE) = 03
+ORDER BY 1, 2, 3;
+
+-- UNION
+
+-- 'UNION ALL'은 중복을 허용하며, 'UNION'은 중복을 제거합니다.
+
+-- NULL AS USER_ID
+
+-- OFFLINE_SALE에는 사용자 ID가 없으므로 NULL 값을 사용합니다.
+
+-- ORDER BY 1, 2, 3
+
+-- 결과를 날짜, 제품 ID, 사용자 ID에 따라 정렬합니다.
+
+-- https://school.programmers.co.kr/learn/courses/30/lessons/131537
