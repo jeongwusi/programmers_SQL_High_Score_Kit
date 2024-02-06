9db82fb240a9d2335e3b1ab296c2daff48b3d520
jeongwusi <jeongwusi@gmail.com>
2024-02-06 15:37:07 +0900

    ✨ feat: `경기도에 위치한 식품창고 목록 출력하기` 문제 해결

--- IS NULL/이름이 없는 동물의 아이디.sql
+++ IS NULL/이름이 없는 동물의 아이디.sql
@@ -6,3 +6,5 @@ ORDER BY ANIMAL_ID;
 -- WHERE NAME IS NULL
 
 -- IS NULL 구문을 통해 NULL 값인지 아닌지 확인할 수 있다.
+
+-- https://school.programmers.co.kr/learn/courses/30/lessons/59039
--- IS NULL/이름이 있는 동물의 아이디.sql
+++ IS NULL/이름이 있는 동물의 아이디.sql
@@ -6,3 +6,5 @@ ORDER BY ANIMAL_ID;
 -- WHERE NAME IS NOT NULL
 
 -- IS NOT NULL 구문을 통해 NULL 값이 아닌 값을 찾을 수 있다.
+
+-- https://school.programmers.co.kr/learn/courses/30/lessons/59407
