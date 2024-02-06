1fdb517878353591c431f3209c699fcce9f50302
jeongwusi <jeongwusi@gmail.com>
2024-02-06 15:49:12 +0900

    ✨ feat: `나이 정보가 없는 회원 수 구하기` 문제 해결

---
+++ IS NULL/나이 정보가 없는 회원 수 구하기.sql
@@ -0,0 +1,13 @@
+SELECT COUNT(*)
+FROM USER_INFO
+WHERE AGE IS NULL;
+
+-- SELECT COUNT(*)
+
+-- COUNT 함수를 이용해서 행 개수 조회
+
+-- WHERE AGE IS NULL;
+
+-- NULL인 값만 조회
+
+-- https://school.programmers.co.kr/learn/courses/30/lessons/131528
