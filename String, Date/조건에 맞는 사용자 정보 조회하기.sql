687d160f50393c7aad2f3bcc1c77a8d08932f1c4
jeongwusi <jeongwusi@gmail.com>
2024-02-07 12:42:56 +0900

    ♻️ refactor: `조건에 맞는 사용자 정보 조회하기` 정리 수정

--- String, Date/조건에 맞는 사용자 정보 조회하기.sql
+++ String, Date/조건에 맞는 사용자 정보 조회하기.sql
@@ -15,9 +15,12 @@ ORDER BY USER_ID DESC;
 
 -- SUBSTR를 이용해서 문자열 자를 수 있다.
 
--- USED_GOODS_BOARD 테이블에서 WRITER_ID를 기준으로 그룹화하고, 각 작성자가 게시한 게시물의 수가 3 이상인 작성자들의 WRITER_ID를 선택합니다. 이는 서브쿼리 (SELECT WRITER_ID FROM USED_GOODS_BOARD GROUP BY WRITER_ID HAVING COUNT(*) >=3) 를 통해 수행됩니다.
--- 선택된 작성자들의 WRITER_ID를 사용하여 USED_GOODS_USER 테이블에서 해당 작성자들의 정보를 가져옵니다.
--- 가져온 정보 중에서 USER_ID, NICKNAME, CITY, STREET_ADDRESS1, STREET_ADDRESS2, TLNO 컬럼 값을 사용하여 새로운 열인 전체주소와 전화번호를 생성합니다. 이때 CONCAT() 함수와 SUBSTR() 함수를 사용하여 주소와 전화번호의 형식을 지정합니다.
--- 최종 결과는 USER_ID를 내림차순으로 정렬하여 반환됩니다.
+-- SUBSTR(TLNO,1,3) -> TLNO에서 1부터 3까지
+
+-- SELECT WRITER_ID FROM USED_GOODS_BOARD GROUP BY WRITER_ID HAVING COUNT(*) >=3
+
+-- USED_GOODS_BOARD 테이블에서 WRITER_ID를 기준으로 그룹화한다. 이렇게 하면 동일한 작성자가 여러 개의 게시물을 작성한 경우에 한 번만 나타나게 된다.
+
+-- 그룹화된 결과에서 COUNT 함수를 사용하여 각 작성자별로 작성한 게시물의 개수를 셀 수 있습니다. 따라서 그룹화된 결과에서 각 작성자가 작성한 게시물의 개수가 3 이상인 경우만 선택합니다.
 
 -- https://school.programmers.co.kr/learn/courses/30/lessons/164670
